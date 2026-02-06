e8f84dfb140fbf0dff94f04f7102631909cff57e
lakshit <your_lakshit2128@gmail.com>
2026-02-06 14:32:12 +0530

    Initial commit

---
+++ day1_intro.py
@@ -0,0 +1,39 @@
+
+print("hello, world!")
+print("I am starting my Gen AI journey")
+print(2+3)
+
+name = "lakshit"
+age = 22
+cgpa = 8.5
+is_student = True
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+print(type(name))
+print(type(age))
+
+name = input("enter your name")
+age = input("enter your age")
+
+print("hello" , name )
+print("your age is", age )
+
+a = 22
+b = 17
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# day1 task
+name = input("tell me your name")
+age = input("tell me your age")
+
+print("soo your name is", name)
+print("and your age is" , age)
+
